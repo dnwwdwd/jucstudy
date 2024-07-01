a93e7bbb216bad0ee25cd4d13521d305d3d63a97
hejiajun <1765393959@qq.com>
2024-07-01 22:28:48 +0800

    add Future学习

--- src/main/java/com/hjj/CompletableFutureDemo.java
+++ src/main/java/com/hjj/CompletableFutureDemo.java
@@ -1,2 +1,23 @@
-package com.hjj;public class CompletableFutureDemo {
+package com.hjj;
+
+import java.util.concurrent.Callable;
+
+public class CompletableFutureDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class MyThread implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
+
+class MyThread2 implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        return null;
+    }
 }
--- src/main/java/com/hjj/DaemonDemo.java
+++ src/main/java/com/hjj/DaemonDemo.java
@@ -1,7 +1,23 @@
 package com.hjj;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.concurrent.TimeUnit;
 
+public class DaemonDemo {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " 开始运行，" +
+                    (Thread.currentThread().isDaemon() ? "守护线程" : "用户线程"));
+            while (true) {
+
+            }
+        }, "t1");
+        t1.setDaemon(false);
+        t1.start();
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " 主线程结束");
     }
 }
